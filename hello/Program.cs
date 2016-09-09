3b8f843799f603e72521447fe5cf90d8c93c490a
AOAE <ae.aoae@hotmail.com>
2016-09-09 14:22:52 +0700

    new

---
+++ hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Watchara 5711423138!");
+
+            // Keep the console window open in debug  mode.
+            Console.WriteLine("pro.");
+            Console.ReadKey();
+        }
+    }
+}
